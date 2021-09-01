73fdf9ba159f35954159018160050e5aebb60a6e
IlarioCOCCO <ilario.cocco@gmail.com>
2021-09-01 17:35:28 +0200

    commit js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // inserire 100 quadratini nel div #campo
 function numeroQuadrati(quadratini) {
     for (var i = 1; i <= quadratini; i++) {
-        document.getElementById("campo").innerHTML += `<div class="quadrato">$i</div>`;
+        document.getElementById("campo").innerHTML += `<div class="quadrato">${i}</div>`;
     }
 }
 var quadratini = prompt("inserisci il numero di Quadratini");
@@ -9,7 +9,10 @@ numeroQuadrati(quadratini);
 
 document.getElementById("campo").addEventListener("click",
     function(event) {
-        event.target.classList.add("click")
+        // l'elemento del dom su cui clicco
+        event.target.classList.add("clicco")
     }
 
 );
+
+console.log(quadratini);
